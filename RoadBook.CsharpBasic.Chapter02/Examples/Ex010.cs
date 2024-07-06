25d5c52f5e5583ecf0cedfaaf273a80d6186dd34
SERO <jooyeonroh@gmail.com>
2024-07-07 04:36:28 +0900

    variable

---
+++ RoadBook.CsharpBasic.Chapter02/Examples/Ex010.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace RoadBook.CsharpBasic.Chapter02.Examples
+{
+    public class Ex010
+    {
+        public void Run()
+        {
+            string strNumber = "10";
+
+            int convertNumber = Convert.ToInt32(strNumber);
+            int parseNumber = Int32.Parse(strNumber);
+
+            Console.WriteLine(
+                "{0} + {1} = {2}",
+                convertNumber,
+                parseNumber,
+                convertNumber + parseNumber
+            );
+        }
+    }
+}
